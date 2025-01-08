aede08d461f12b65940e3ab3e0c7b6804de75d12
unnamed <depa@logique.co.id>
2025-01-08 02:20:57 +0000

    initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://forum.katalon.com/');
+  await page.getByPlaceholder('Search').click();
+  await page.getByPlaceholder('Search').fill('testops - bypass');
+  await page.getByPlaceholder('Search').press('Enter');
+  await page.getByRole('link', { name: 'TestOps - Bypass of Disabled Input Fields via HTML Manipulation Bugs Report 19h' }).locator('a').click();
+  await page.getByLabel('Katalon TestOps link clicked').click();
+  await expect(page.locator('div').filter({ hasText: /^Log in$/ })).toBeVisible();
+});
