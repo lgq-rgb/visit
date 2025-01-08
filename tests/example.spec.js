d07467eb0fde28b829295a74e9bb8e2833203fd9
unnamed <depa@logique.co.id>
2025-01-08 03:51:49 +0000

    fix failed test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,7 +5,7 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('Search').click();
   await page.getByPlaceholder('Search').fill('testops - bypass');
   await page.getByPlaceholder('Search').press('Enter');
-  await page.getByRole('link', { name: 'TestOps - Bypass of Disabled Input Fields via HTML Manipulation Bugs Report 19h' }).locator('a').click();
+  await page.getByRole('link', { name: 'TestOps - Bypass of Disabled Input Fields via HTML Manipulation Bugs Report' }).locator('a').click();
   await page.getByLabel('Katalon TestOps link clicked').click();
   await expect(page.locator('div').filter({ hasText: /^Log in$/ })).toBeVisible();
 });
